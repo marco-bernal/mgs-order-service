c27ba13350e15e1a4b31e0839ec1b028f42678a5
marco-bernal <bernal.marco.a.84@gmail.com>
2024-10-27 16:08:44 -0600

    IS-2 - Refactoring code and small improvements

--- src/main/java/com/mgs/order/OrderServiceApplication.java
+++ src/main/java/com/mgs/order/OrderServiceApplication.java
@@ -1,4 +1,4 @@
-package com.template.order;
+package com.mgs.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/mgs/order/OrderServiceApplicationTests.java
+++ src/test/java/com/mgs/order/OrderServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.template.order;
+package com.mgs.order;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
